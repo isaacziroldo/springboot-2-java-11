9e5e94d6feb71dcc2bcbab6bdc4de94fb56ccf03
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-05-20 00:16:49 -0300

    Service layer, component registratio

--- src/main/java/com/example/webcourse/services/UserService.java
+++ src/main/java/com/example/webcourse/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.webcourse.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.webcourse.entities.User;
 import com.example.webcourse.repositories.UserRepository;
+
 
 @Service
 public class UserService {
@@ -16,6 +18,15 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+		
+		
 	}
 
 }
