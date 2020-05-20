ba222d52bc8a7838d5cbe4ad71bb54cd08dc6e03
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-05-19 23:37:02 -0300

    teste

--- src/main/java/com/example/webcourse/resources/UserResource.java
+++ src/main/java/com/example/webcourse/resources/UserResource.java
@@ -1,21 +1,29 @@
 package com.example.webcourse.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webcourse.entities.User;
+import com.example.webcourse.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Isaac", "isaaclfr@gmail.com", "11940293141", "pnc741741");
+	public ResponseEntity<List<User>> findAll(){
 		
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
 	}
 
 }
