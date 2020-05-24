34e57435f3016a519a4afe9459326c3fa6cff477
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-05-24 11:08:09 -0300

    config

--- src/main/java/com/example/webcourse/config/TestConfig.java
+++ src/main/java/com/example/webcourse/config/TestConfig.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.example.webcourse.entities.User;
 import com.example.webcourse.repositories.UserRepository;
@@ -19,7 +18,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u1 = new User(null, "Joao", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Julio Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Tito Pirulito", "alex@gmail.com", "977777777", "123456");
 		
