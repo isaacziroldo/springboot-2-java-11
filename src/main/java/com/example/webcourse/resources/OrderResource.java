76eec40065d8ad97537ee484cd5cc60cf30308d2
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-05-25 11:18:14 -0300

    Order, Instant, ISO8601 - PART2

--- src/main/java/com/example/webcourse/resources/OrderResource.java
+++ src/main/java/com/example/webcourse/resources/OrderResource.java
@@ -11,27 +11,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.webcourse.entities.User;
-import com.example.webcourse.services.UserService;
+import com.example.webcourse.entities.Order;
+import com.example.webcourse.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 	
 	@Autowired
-	private UserService service;
+	private OrderService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<Order>> findAll(){
 		
-		List<User> list = service.findAll();
+		List<Order> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable long id){
-		User obj = service.findById(id);
+	public ResponseEntity<Order> findById(@PathVariable long id){
+		Order obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
